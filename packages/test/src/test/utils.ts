f75d7391a65436b6c1f0c02fbd3dd7d4392b3675
Jos Warmer <jos.warmer@openmodeling.nl>
2025-07-09 09:10:08 +0200

    Starting omn tests

--- packages/test/src/test/utils.ts
+++ packages/test/src/test/utils.ts
@@ -1,5 +1,6 @@
 import { LionWebJsonChunk } from "@lionweb/json"
 import { LionWebJsonChunkWrapper } from "@lionweb/json-utils"
+import { CommandType } from "@lionweb/server-delta-shared"
 import fs from "fs"
 
 export function readModel(filename: string): LionWebJsonChunk | null {
@@ -8,6 +9,17 @@ export function readModel(filename: string): LionWebJsonChunk | null {
         if (stats.isFile()) {
             const chunk: LionWebJsonChunk = JSON.parse(fs.readFileSync(filename).toString())
             return chunk
+        }
+    }
+    return null
+}
+
+export function readDelta(filename: string): CommandType | null {
+    if (fs.existsSync(filename)) {
+        const stats = fs.statSync(filename)
+        if (stats.isFile()) {
+            const cmd: CommandType = JSON.parse(fs.readFileSync(filename).toString())
+            return cmd
         }
     }
     return null
