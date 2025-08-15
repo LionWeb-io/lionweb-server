a7c012065ab4b63d59d70a8855472154f4f55dc8
ftomassetti <federico@strumenta.com>
2025-08-15 16:08:06 +0200

    update tests on bulkImport

---
+++ packages/test/src/test/additional-api-tests.ts
@@ -0,0 +1,127 @@
+import { HttpSuccessCodes } from "@lionweb/server-shared"
+import { RepositoryClient, TransferFormat } from "@lionweb/server-client"
+import { LionWebJsonChunk } from "@lionweb/json"
+import { LionWebJsonDiff } from "@lionweb/json-diff"
+
+import { assert } from "chai"
+import sm from "source-map-support"
+import { BulkImport } from "@lionweb/server-additionalapi"
+
+const { deepEqual, fail } = assert
+
+sm.install()
+
+describe("Client - Additional API tests", () => {
+    const repository = "clientAdditionalAPIRepo";
+    const client = new RepositoryClient("TestClient", repository)
+    let initError: string = ""
+
+    before("create database", async function () {
+        const initResponse = await client.dbAdmin.createDatabase()
+        if (initResponse.status !== HttpSuccessCodes.Ok) {
+            console.log("Cannot create database: " + JSON.stringify(initResponse.body))
+        } else {
+            console.log("database created: " + JSON.stringify(initResponse.body))
+        }
+    })
+
+    beforeEach("a", async function () {
+        client.repository = repository
+        initError = ""
+        const initResponse = await client.dbAdmin.createRepository(repository, false, "2023.1")
+        if (initResponse.status !== HttpSuccessCodes.Ok) {
+            console.log("Cannot initialize database: " + JSON.stringify(initResponse.body))
+            initError = JSON.stringify(initResponse.body)
+            return
+        } else {
+            console.log("initialized database: " + JSON.stringify(initResponse.body))
+        }
+        const repositories = await client.dbAdmin.listRepositories()
+        console.log("repositories: " + JSON.stringify(repositories.body.repositories))
+    })
+
+    afterEach("a", async function () {
+        await client.dbAdmin.deleteRepository(repository)
+    })
+
+    describe("Bulk import", () => {
+        const transferFormats : TransferFormat[] = [TransferFormat.JSON, TransferFormat.FLATBUFFERS];
+        transferFormats.forEach(format => {
+            it(`bulk import, no compression, ${format}`, async function () {
+                this.timeout(15_000)
+                assert(initError === "", initError)
+
+                const bulkImport: BulkImport = {
+                    attachPoints: [],
+                    nodes: [
+                        {
+                            id: "bi-id1",
+                            classifier: {
+                                language: "bi-language",
+                                version: "v1.1",
+                                key: "bi-language-key"
+                            },
+                            properties: [
+                                {
+                                    property: {
+                                        language: "bi-language",
+                                        version: "v1.1",
+                                        key: "bi-p1"
+                                    },
+                                    value: "qwerty"
+                                }
+                            ],
+                            containments: [],
+                            references: [
+                                {
+                                    reference: {
+                                        language: "bi-language",
+                                        version: "v1.1",
+                                        key: "bi-r1"
+                                    },
+                                    targets: [
+                                        {
+                                            reference: "id-a",
+                                            resolveInfo: "resolveInfo-a"
+                                        },
+                                        {
+                                            reference: "id-b",
+                                            resolveInfo: "resolveInfo-b"
+                                        }
+                                    ]
+                                }
+                            ],
+                            annotations: [],
+                            parent: null
+                        }
+                    ]
+                }
+
+                const bulkImportResult = await client.additional.bulkImport(bulkImport, format, false)
+                if (bulkImportResult.status !== HttpSuccessCodes.Ok) {
+                    const errMsg = "Cannot create partition using bulk import: " + JSON.stringify(bulkImportResult.body)
+                    console.error(errMsg)
+                    const err = JSON.stringify(bulkImportResult.body)
+                    fail(err)
+                }
+                // Now retrieve the partition again.
+                const partitions = await client.bulk.retrieve(["bi-id1"])
+                const diff = new LionWebJsonDiff()
+                const expectedModel: LionWebJsonChunk = {
+                    serializationFormatVersion: "2023.1",
+                    languages: [
+                        {
+                            key: "bi-language",
+                            version: "v1.1"
+                        }
+                    ],
+                    nodes: bulkImport.nodes
+                }
+                diff.diffLwChunk(expectedModel, partitions.body.chunk)
+                deepEqual(diff.diffResult.changes, [])
+
+                await client.bulk.deletePartitions(["bi-id1"])
+            })
+        })
+    })
+})
